702882b4d7a072ff11bae77a8cb2879d42da6561
EgorSavchuk <egor_savchuk_2016@mail.ru>
2021-12-11 03:11:39 +0500

    version with html-answer

--- autoMOZI.py
+++ autoMOZI.py
@@ -9,10 +9,8 @@ if __name__ == '__main__':
     task3 = str(input("Решить задачу 2 y/n: "))
     task4 = str(input("Решить задачу 3 y/n: "))
     if task2 == 'y':
-        from task2 import get_answer_task2
         cardinality = int(input("Задача с исследованием абстрактной циклической группы: Введите порядок группы: "))
     if task3 == 'y':
-        from task3 import get_result_task3
         substitution1 = input("Задача 2: Первая подстановка, введите только числа второй строки через пробел: ").split()
         substitution1 = list(map(int, substitution1))
         substitution2 = input("Задача 2: Вторая подстановка, введите только числа второй строки через пробел: ").split()
@@ -21,8 +19,8 @@ if __name__ == '__main__':
         pow_substitution2 = int(input("Задача 2: Введите степень второй подстановки: "))
         pow_general = int(input("Задача 2: Введите общую степень подстановок, если такой нет в задании введите 1: "))
     if task4 == 'y':
-        from task4 import get_answer_for_task4
         p = int(input("Задача 3: Введите Z_: "))
+
     if '-html' in sys.argv:
         file_loader = FileSystemLoader('Templates')
         env = Environment(loader=file_loader)
